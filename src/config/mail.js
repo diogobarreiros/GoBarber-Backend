05a263d74393a13a6266b85261fcc94b42ef7c9b
diogobarreiros <diogo.noroes@gmail.com>
2019-10-30 13:27:40 -0300

    Enviar email na criação de agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '76d188ba4a94a0',
+    pass: 'da80eecbc1cbe4',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
